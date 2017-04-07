863d2583873790dedf1617af2ec6072db7a08693
peach_mr@sina.com <peach_mr@sina.com>
2017-04-07 17:33:52 +0800

    移除spring依赖

--- src/test/java/com/peachyy/logback/Test1.java
+++ src/test/java/com/peachyy/logback/Test1.java
@@ -1,7 +1,12 @@
 package com.peachyy.logback;
 
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * <p>描述:</p>
@@ -13,7 +18,7 @@ import org.slf4j.LoggerFactory;
 public class Test1 {
 
     public static Logger log = LoggerFactory.getLogger(Test1.class.getName());
-
+    ExecutorService executors = Executors.newFixedThreadPool(1000);
     public static void main(String[] args) {
         for (int i = 0; i < 1000; i++) {
             log.debug("测试一下1{}", i);
@@ -26,5 +31,24 @@ public class Test1 {
             }
         }
 
+    }
+
+    @Test
+    public void test2() {
+        for (int i = 0; i < 1000; i++) {
+            final String a = Objects.toString(i);
+            executors.execute(new Runnable() {
+                @Override
+                public void run() {
+                    log.debug("测试一下{}", a);
+                }
+            });
+
+        }
+//        try {
+//            Thread.sleep(111111111);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
 }
